5bca404b1a9ddcfe28acdc680b7ef73b2c998c4a
Necriptos <lulzgamestudio@gmail.com>
2017-06-10 20:16:14 +0200

    15.3 Update
    updated to 15.3

---
+++ bobmods_gfxtweak_0.15.3/settings.lua
@@ -0,0 +1,24 @@
+data:extend(
+{
+  {
+    type = "bool-setting",
+    name = "replace-electronics",
+    setting_type = "startup",
+    default_value = true,
+  },
+  {
+    type = "bool-setting",
+    name = "replace-warfare",
+    setting_type = "startup",
+    default_value = true,
+  },
+  {
+    type = "bool-setting",
+    name = "replace-logistics",
+    setting_type = "startup",
+    default_value = true,
+  },
+}
+)
+
+
