f43044f2a7e0fbcde258b85512738c08d05520fb
Necriptos <lulzgamestudio@gmail.com>
2019-11-11 11:41:13 +0100

    .17 update
    Updated to .17, Removed robots because default ones are better, changed code to allow my 32px picture to work instead of the 128px default ones.

---
+++ bobmods_gfxtweak_0.17.0/settings.lua
@@ -0,0 +1,18 @@
+data:extend(
+{
+  {
+    type = "bool-setting",
+    name = "replace-electronics",
+    setting_type = "startup",
+    default_value = true,
+  },
+  {
+    type = "bool-setting",
+    name = "replace-warfare",
+    setting_type = "startup",
+    default_value = true,
+  },
+}
+)
+
+
